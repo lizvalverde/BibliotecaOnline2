370940c1ca5f771087c1ac1662e72e74e179dac5
Dani <daniel@polaris.es>
2021-10-27 22:19:34 +0200

    cambiado jquery y metidas imagenes

--- WebBibliotecaOnline2/App_Start/BundleConfig.cs
+++ WebBibliotecaOnline2/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace WebBibliotecaOnline2
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                       "~/Scripts/jquery-3.6.0.min.js"));
+                       "~/Scripts/jquery-1.10.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
